2d404c7c7fc6220f2cc5aa129e30a9dd34432f8d
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-21 22:27:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,9 +21,10 @@ def monopoly_with_third_tier
   # complexity.
   newHash = monopoly_with_second_tier
   newHash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
-  newHash[:railroads][:names] = {}
+  newHash[:railroads][:names] = {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
   newHash
 end
+
 
 def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
