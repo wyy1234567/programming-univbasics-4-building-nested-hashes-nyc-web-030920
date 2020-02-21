0cd9a04ea225d79ff8efcb7751dadaae0d7b997a
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-21 22:28:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,4 +31,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  newHash = monopoly_with_third_tier
+  newHash[:railroads][:names][:reading_railroad][""]
+  newHash
 end
