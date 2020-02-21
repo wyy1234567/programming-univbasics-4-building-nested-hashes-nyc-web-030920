43aa6a106c4b2a92a8e4e502be913c87e37f0933
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-21 22:18:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,6 +20,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   newHash = monopoly_with_second_tier
+  newHash[:railroads][:rent_in_dollars] = {}
   
 end
 
